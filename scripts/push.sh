141407f01cbf5a757278b24d1896c5bceb278749
philipsz-davido <philipsz-davido>
2018-04-10 15:38:56 +0100

    Added Observable Observer Subscriber BehaviorSubject etc

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
 
+#if $(git push origin master); 
+#then
+#    echo 'yea'
+#else
+#    echo 'no'
+#fi
+
 git add . && git commit -a -m 'Added Observable Observer Subscriber BehaviorSubject etc' 
 git push origin master
+
